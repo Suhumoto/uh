bd877ee9320811d16023370652f4b1e6f206de6c
Suhumoto <154435728+Suhumoto@users.noreply.github.com>
2024-12-01 16:14:56 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        return s.equals(new StringBuilder(s).reverse().toString());
+    class Book {
+        private String title;
+        private String author;
+        private double price;
+
+        public Book(String title, String author, double price) {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+
+        public void applyDiscount(double percent) {
+            this.price -= this.price * (percent / 100);
+        }
+
+        public void printInfo() {
+            System.out.printf("Назва: %s, Автор: %s, Ціна: %.2f грн%n", title, author, price);
+        }
     }
 
-    public static int countVowels(String s) {
-        String vowels = "aeiou";
-        s = s.toLowerCase();
-        int count = 0;
-        for (char c : s.toCharArray()) {
-            if (vowels.indexOf(c) != -1) {
-                count++;
-            }
+    public class bruh {
+        public void main(String[] args) {
+            Book book1 = new Book("Війна і мир", "Лев Толстой", 500);
+            Book book2 = new Book("Майстер і Маргарита", "Михайло Булгаков", 300);
+            Book book3 = new Book("Тарас Бульба", "Микола Гоголь", 200);
+
+            book3.applyDiscount(10);
+
+            book1.printInfo();
+            book2.printInfo();
+            book3.printInfo();
         }
-        return count;
     }
 }
+
